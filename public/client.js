1be9a70079e779d46a474459d37d66ccc499f5f7
Sebastien P <maxymeum.prod@gmail.com>
2011-05-08 19:13:21 +0200

    Easier this way :(

--- public/client.js
+++ public/client.js
@@ -10,45 +10,6 @@
 */
 
 
-// https://github.com/douglascrockford/JSON-js/blob/master/json2.js
-if (!(window.JSON && (typeof JSON.stringify === "function"))) {
-
-	window.JSON = {
-		stringify: function (value, replacer, space) {
-
-			var i;
-				gap = "";
-				indent = "";
-
-			if (typeof space === "number") {
-
-				for (i = 0; i < space; i += 1) {
-
-	                    indent += " ";
-
-				}
-
-			} else if (typeof space === "string") {
-
-				indent = space;
-
-			}
-
-			rep = replacer;
-
-			if (replacer && typeof replacer !== "function" && (typeof replacer !== "object" || typeof replacer.length !== "number")) {
-
-				throw new Error("JSON.stringify");
-
-			}
-
-			return str("", { "": value });
-        
-		}
-	};
-
-}
-
 $(function () {
 
 	var $dialog = $("#dialog").dialog({ autoOpen: !1, modal: !0 }),
